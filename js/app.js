bcf512ef2e719470f3c8e1726e786a7709eec6ba
Meherab211 <meherab395@gmail.com>
2022-03-11 21:23:40 +0600

    done all

--- js/app.js
+++ js/app.js
@@ -41,6 +41,7 @@ const displayContent = (text) => {
 
 const switchTab = (id) => {
     if (id === "posts") {
+      document.getElementById('bonous-part').style.display="block"
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
